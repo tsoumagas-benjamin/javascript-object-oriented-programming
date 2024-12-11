032345f038ee7dfc3306ac3d4b88c7b5822a1690
tsoumagas-benjamin <bptsoumagas@gmail.com>
2024-12-11 15:00:27 -0500

    Created PerishableProductProperties subclass in js

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ class ProductProperties {
         return `Product: ${this.name}, Price: \$${this.price}, Quantity: ${this.quantity}`;
     }
 }
+
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: \$${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
+    }
+}
