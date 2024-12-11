cc09bc6ec30c28e0180910b97ba29959b51c93f7
tsoumagas-benjamin <bptsoumagas@gmail.com>
2024-12-11 15:04:29 -0500

    Created 3 Perishable Products in js

--- script.js
+++ script.js
@@ -24,3 +24,7 @@ class PerishableProductProperties extends ProductProperties {
         return `Product: ${this.name}, Price: \$${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
 }
+
+const product1 = new PerishableProductProperties("Tuna", 2.99, 5, "2025-2-15");
+const product2 = new PerishableProductProperties("Banana", 1.50, 7, "2024-12-18");
+const product3 = new PerishableProductProperties("Milk", 3.99, 2, "2024-12-25");
